9ada0b3d72a887f964623176ef0040efe0ccbc3f
Wouter J <waldio.webdesign@gmail.com>
2013-01-22 21:03:59 +0100

    Improved the PHP code

--- fluid-grids.php
+++ fluid-grids.php
@@ -1,17 +1,27 @@
 <?php
 
     // How many columns would you like in your grid system?
-    $totalCols   = 16;
+    define('TOTAL_COLS', 16);
 
     // How wide is one column?
-    $colWidth    = 40;
+    define('COL_WIDTH', 40);
 
     // How wide is one gutter?
-    $gutterWidth = 20;
+    define('GUTTER_WIDTH', 20);
 
     // We work these next two out for you, you don’t need to touch them.
-    $totalWidth  = $totalCols * ($colWidth + $gutterWidth);    
-    $fluidGutterWidth = round(($gutterWidth / $totalWidth) * 100, 3);
+    $totalWidth  = TOTAL_COLS * (COL_WIDTH + GUTTER_WIDTH);    
+    $fluidGutterWidth = round((GUTTER_WIDTH / $totalWidth) * 100, 3);
+    
+    /**
+     * The number of columns into the loop, plus the corresponding number
+     * of gutters, minus one gutter (which is taken up as `margin-left`)
+     * converted to a percentage (to three decimal places).
+     */
+    function calculateWidth($column)
+    {    
+        return round((((($i * $colWidth) + ($i * $gutterWidth)) - $gutterWidth) / $totalWidth) * 100, 3);
+    }
 
 ?>
 
@@ -25,25 +35,9 @@
     float:left;
     margin-left:<?php echo $fluidGutterWidth . '%;<br>'; ?>
 }
-<?php
-
-    // Loop through all our possible column widths:
-    for($i = 1; $i <= $totalCols; $i++){
-        
-        echo '.grid-' . $i . '{ ';
-        
-        /**
-         * The number of columns into the loop, plus the corresponding number
-         * of gutters, minus one gutter (which is taken up as `margin-left`)
-         * converted to a percentage (to three decimal places).
-         */
-        $cssWidth = round((((($i * $colWidth) + ($i * $gutterWidth)) - $gutterWidth) / $totalWidth) * 100, 3);
-        
-        echo 'width:' . $cssWidth . '%;';
-        
-        
-        echo ' }<br>';
-        
-    }
-?>
+<?php for($i = 1; $i <= $totalCols; $i++) : ?>   
+.grid-<?php echo $i ?>{
+    width:<?php echo calculateWidth($i) ?>%;
+}
+<?php endfor ?>
 </pre>
