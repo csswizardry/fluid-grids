8c178c1a6b0c7b88b17f5124ec57214e785dcc0b
James Hall <james@snapshotmedia.co.uk>
2013-01-23 00:50:24 +0000

    Fix rounding function

--- fluid-grids.js
+++ fluid-grids.js
@@ -9,36 +9,46 @@ var FluidGrids = function() {
 	 * Private properties
 	 */
 
-    var getVal = function(selector) {
-        return parseInt($(selector).val(), 10);
-    };
+	var round = function(value, precision) {
 
-    var calculate = function() {
-        var totalWidth  = getVal('#total-cols') * (getVal('#total-cols') + getVal('#gutter-width'));    
-        var fluidGutterWidth = Math.round((getVal('#gutter-width') / totalWidth) * 100, 3);
+		// Multiply number by amount we want our round precision to be
+		var power = Math.pow(10, precision);
+		var number = power * value;
+
+		return Math.round(number) / power;
+
+	};
+
+	var getVal = function(selector) {
+		return parseInt($(selector).val(), 10);
+	};
+
+	var calculate = function() {
+
+		var data = {
+			totalCols: getVal('#total-cols'),
+			colWidth: getVal('#col-width'),
+			gutterWidth: getVal('#gutter-width')
+		};
+
+		data.totalWidth  = data.totalCols * (data.colWidth + data.gutterWidth);	
+		data.fluidGutterWidth = round((data.gutterWidth / data.totalWidth) * 100, 3);
 
 		var template = swig.compile($('#css-template').html());
 
 		var cols = [];
-		for (i = 0; i < getVal('#total-cols'); i++) {
+		for (i = 1; i <= data.totalCols; i++) {
 			
-			var totalColWidth = i * getVal('#col-width');
-	        totalGutterWidth = i * getVal('#gutter-width');
-	        finalGutterWidth = (totalColWidth + totalGutterWidth) - getVal('#gutter-width');
-	        
-	        cssWidth = Math.round((finalGutterWidth / totalWidth) * 100, 3);
+			var cssWidth = round(
+				((((i * data.colWidth) + (i * data.gutterWidth)) - data.gutterWidth) / data.totalWidth) * 100, 3
+			);
+
 			cols.push(cssWidth);
 		}
 
-        var output = template({
-        	totalCols: getVal('#total-cols'),
-        	colWidth: getVal('#col-width'),
-        	gutterWidth: getVal('#gutter-width'),
-        	totalWidth: totalWidth,
-        	fluidGutterWidth: fluidGutterWidth,
-        	cols: cols
-        });
-        $('textarea').val(output);
+		data.cols = cols;
+		var output = template(data);
+		$('textarea').val(output);
 	};
 
 	/**
@@ -47,11 +57,11 @@ var FluidGrids = function() {
 	return {
 		init: function() {
 			// Attach event handlers
-			console.log('hi there');
 			$('#total-cols, #col-width, #gutter-width').change(function() {
-				console.log('calc');
 				calculate();
 			});
+
+			calculate();
 		}
 	};
 }();
